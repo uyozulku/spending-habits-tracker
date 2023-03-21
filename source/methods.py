ba682b13eb036152617ae9fc2dee678e3c6cca20
Unal Yigit Ozulku <uyozulku@gmail.com>
2023-03-20 22:09:19 -0400

    more helpers

--- source/methods.py
+++ source/methods.py
@@ -74,3 +74,61 @@ def get_accounts(access_token):
     # return json object
     return json.dumps(accounts_response['accounts'][:2], indent=4, sort_keys=True)
 
+# get balance
+def get_balance(access_token):
+    # get balance from plaid api
+    # return json object
+
+    # create client object
+    client = plaid.Client(
+        client_id=os.getenv('PLAID_CLIENT_ID'),
+        secret=os.getenv('PLAID_SECRET'),
+        environment=os.getenv('PLAID_ENV'),
+        api_version='2020-09-14'
+    )
+
+    # get balance
+    balance_response = client.Accounts.balance.get(access_token)
+
+    # return json object
+    return json.dumps(balance_response['accounts'][:2], indent=4, sort_keys=True)
+
+
+# get identity
+def get_identity(access_token):
+    # get identity from plaid api
+    # return json object
+
+    # create client object
+    client = plaid.Client(
+        client_id=os.getenv('PLAID_CLIENT_ID'),
+        secret=os.getenv('PLAID_SECRET'),
+        environment=os.getenv('PLAID_ENV'),
+        api_version='2020-09-14'
+    )
+
+    # get identity
+    identity_response = client.Identity.get(access_token)
+
+    # return json object
+    return json.dumps(identity_response['accounts'][:2], indent=4, sort_keys=True)
+
+# get income
+def get_income(access_token):
+    # get income from plaid api
+    # return json object
+
+    # create client object
+    client = plaid.Client(
+        client_id=os.getenv('PLAID_CLIENT_ID'),
+        secret=os.getenv('PLAID_SECRET'),
+        environment=os.getenv('PLAID_ENV'),
+        api_version='2020-09-14'
+    )
+
+    # get income
+    income_response = client.Income.get(access_token)
+
+    # return json object
+    return json.dumps(income_response['accounts'][:2], indent=4, sort_keys=True)
+
