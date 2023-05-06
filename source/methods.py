d94116e78833f70772b360a1702f0ba1aa48f930
Unal Yigit Ozulku <uyozulku@gmail.com>
2023-05-06 01:16:06 -0400

    Squashed commit of the following:
    commit 2861e12886f184c3d143efd38b451dcfb3b04834
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Sat May 6 00:55:32 2023 -0400
    
        format
    
    commit 39fee0b2bd4193a699654b34e175ca10d274b56a
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Sat May 6 00:54:01 2023 -0400
    
        no matplotlib
    
    commit 34d43c23d1286970724c62a16cc52335696238c8
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Sat May 6 00:52:27 2023 -0400
    
        comment out graph stuff for now
    
    commit ecd438ade3160b3c4e40bc8115826fc5b4325163
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Sat May 6 00:49:51 2023 -0400
    
        manifest
    
    commit 711dd4a5f6a61ff664c3b71367ab8247604efb75
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Sat May 6 00:45:22 2023 -0400
    
        remove 2nd import
    
    commit d8a47af66a77139878a79257870fa9e80f99295d
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Fri May 5 23:38:10 2023 -0400
    
        new stuff
    
    commit 98df0b103ea94e9052fdb1ea715e7f39da51a097
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 22:40:16 2023 -0400
    
        delete vsc file
    
    commit c4f10af83b0146f0130c642c649927c15d7d959f
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 22:37:10 2023 -0400
    
        more changes
    
    commit 7fee605340662fd477cf170f560ca189cd6db786
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:57:19 2023 -0400
    
        toml import external libraries
    
    commit 0a8ec01436bdd458808654dd2dd92166c1eecc76
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:31:30 2023 -0400
    
        readme edit
    
    commit 22606e7b93c326ba39f946303406b58dc6c3e4a3
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:29:09 2023 -0400
    
        readme edit
    
    commit 4b64e8a0597108e3f7d05d22cec6229b7db37f04
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:21:29 2023 -0400
    
        rename test
    
    commit 941aaae0405266b674c6c150f7e6c74e5a9f2938
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:16:44 2023 -0400
    
        toml edits
    
    commit 1b8dc000e7088b990ceb54837aa6a4e74f9a0ca8
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:11:46 2023 -0400
    
        add init to test
    
    commit 0082723421851ba600171235803b42047e9ffd6a
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 03:00:43 2023 -0400
    
        edit manifest
    
    commit 7f60deba1acdb051e196ca73d79011a767549bda
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 02:58:49 2023 -0400
    
        manifest
    
    commit b92d33a07c3225eb16e03e7fdd6810e7c3e36ec8
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 02:54:41 2023 -0400
    
        remove redundant import
    
    commit 37b5a2796b122ba89178f6624a80a36d5e41735d
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 02:52:20 2023 -0400
    
        lint
    
    commit af26dbb175ec0970e1a834a21b9bacdd6ba5fb4b
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 02:50:48 2023 -0400
    
        edit toml
    
    commit 1620250e303cce4dfbe7c102f84d41030f264b1f
    Author: Unal Yigit Ozulku <uyozulku@gmail.com>
    Date:   Wed May 3 02:39:42 2023 -0400
    
        edit toml

--- source/methods.py
+++ source/methods.py
@@ -1,133 +1,196 @@
-# some helper functions
-# 1) get access token
-# 2) get transactions
-# 3) get accounts
+# library of methods to track personal spending habits and trends
+
+import pandas as pd
+
+# import matplotlib.pyplot as plt
 
 
-import plaid
-import json
-import os
-from dotenv import load_dotenv
+def get_data():
+    """
+    Reads in data from csv file
+    """
+    data = pd.read_csv('./data.csv')
+    return data
 
-load_dotenv()
 
-# get access token
-def get_access_token():
-    # get access token from plaid api
-    # return access token
+def get_categories(data):
+    """
+    Returns a list of categories in the data.
 
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
+    Parameters:
+    data (pandas.DataFrame): Input data.
+
+    Returns:
+    categories (list): List of categories in the data.
+    """
+    categories = data.Category.unique().tolist()
+    categories.sort()  # sort the list of categories
+    return categories
+
+
+def get_category_data(data, category):
+    """
+    Returns a dataframe of all transactions in a given category
+    """
+    return data[data['Category'] == category]
+
+
+def get_category_totals(data):
+    """
+    Returns a dataframe of the total amount spent in each category
+    """
+    return data.groupby('Category').sum()
+
+
+def get_category_averages(df):
+    grouped_df = (
+        df.groupby(['Category'])
+        .agg({'Amount': 'mean', 'Date': 'count'})
+        .rename(columns={'Amount': 'Average Amount', 'Date': 'Transaction Count'})
+    )
+    return grouped_df.reset_index()
+
+
+def get_category_counts(data):
+    """
+    Returns a dataframe of the number of transactions in each category
+    """
+    return data.groupby('Category').count()
+
+
+def get_category_stats(data):
+    """
+    Returns a dataframe of the total amount, average amount, and number of transactions in each category
+    """
+    return data.groupby('Category').agg(
+        Amount=pd.NamedAgg(column='Amount', aggfunc='sum'),
+        Amount_x=pd.NamedAgg(column='Amount', aggfunc='mean'),
+        Amount_y=pd.NamedAgg(column='Amount', aggfunc='count'),
     )
 
-    # create public token
-    create_tkn_response = client.Sandbox.public_token.create(
-        os.getenv('PLAID_INSTITUTION_ID'), ['transactions'])
 
-    # exchange public token for access token
-    exchange_response = client.Item.public_token.exchange(
-        create_tkn_response['public_token'])
+def get_category_stats_by_month(data):
+    df = data.copy()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
 
-    # return access token
-    return exchange_response['access_token']
+    # create a multi-level index with category and month
+    df.index = [df.index.year, df.index.month, df['Category']]
+    df.index.names = ['Year', 'Month', 'Category']
 
-# get transactions
-def get_transactions(access_token):
-    # get transactions from plaid api
-    # return json object
-
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
+    # group the data by category and month, and sum the amounts
+    return (
+        df.groupby(level=['Year', 'Month', 'Category'])['Amount']
+        .agg(['sum', 'mean', 'count'])
+        .rename(columns={'sum': 'Amount', 'mean': 'Amount_x', 'count': 'Amount_y'})
     )
 
-    # get transactions
-    transactions_response = client.Transactions.get(access_token, '2020-01-01', '2020-09-14')
 
-    # return json object
-    return json.dumps(transactions_response['transactions'][:2], indent=4, sort_keys=True)
-
-
-# get accounts
-def get_accounts(access_token):
-    # get accounts from plaid api
-    # return json object
-
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
+def get_category_stats_by_year(data):
+    """
+    Returns a dataframe of the total amount, average amount, and number of transactions in each category by year
+    """
+    data = data.copy()
+    data['Year'] = pd.to_datetime(data['Date']).dt.year
+    data = data.groupby(['Category', 'Year']).agg(
+        Amount=pd.NamedAgg(column='Amount', aggfunc='sum'),
+        Amount_x=pd.NamedAgg(column='Amount', aggfunc='mean'),
+        Amount_y=pd.NamedAgg(column='Amount', aggfunc='count'),
     )
-
-    # get accounts
-    accounts_response = client.Accounts.get(access_token)
-
-    # return json object
-    return json.dumps(accounts_response['accounts'][:2], indent=4, sort_keys=True)
-
-# get balance
-def get_balance(access_token):
-    # get balance from plaid api
-    # return json object
-
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
-    )
-
-    # get balance
-    balance_response = client.Accounts.balance.get(access_token)
-
-    # return json object
-    return json.dumps(balance_response['accounts'][:2], indent=4, sort_keys=True)
+    return data.reset_index()
 
 
-# get identity
-def get_identity(access_token):
-    # get identity from plaid api
-    # return json object
+# # generate a pie chart of the total amount spent in each category
+# def pie_chart(data):
+#     """
+#     Generates a pie chart of the total amount spent in each category
+#     """
+#     totals = get_category_totals(data)
+#     labels = totals.index
+#     plt.pie(totals['Amount'], labels=labels, autopct='%1.1f%%')
+#     plt.title('Total Amount Spent in Each Category')
+#     plt.show()
 
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
-    )
 
-    # get identity
-    identity_response = client.Identity.get(access_token)
+# # generate a bar chart of the total amount spent in each category
+# def bar_chart(data):
+#     """
+#     Generates a bar chart of the total amount spent in each category
+#     """
+#     totals = get_category_totals(data)
+#     labels = totals.index
+#     plt.bar(labels, totals['Amount'])
+#     plt.title('Total Amount Spent in Each Category')
+#     plt.show()
 
-    # return json object
-    return json.dumps(identity_response['accounts'][:2], indent=4, sort_keys=True)
 
-# get income
-def get_income(access_token):
-    # get income from plaid api
-    # return json object
+# # generate a pie chart of the average amount spent in each category
+# def pie_chart_averages(data):
+#     """
+#     Generates a pie chart of the average amount spent in each category
+#     """
+#     averages = get_category_averages(data)
+#     labels = averages.index
+#     plt.pie(averages['Amount'], labels=labels, autopct='%1.1f%%')
+#     plt.title('Average Amount Spent in Each Category')
+#     plt.show()
 
-    # create client object
-    client = plaid.Client(
-        client_id=os.getenv('PLAID_CLIENT_ID'),
-        secret=os.getenv('PLAID_SECRET'),
-        environment=os.getenv('PLAID_ENV'),
-        api_version='2020-09-14'
-    )
 
-    # get income
-    income_response = client.Income.get(access_token)
+# # generate a bar chart of the average amount spent in each category
+# def bar_chart_averages(data):
+#     """
+#     Generates a bar chart of the average amount spent in each category
+#     """
+#     averages = get_category_averages(data)
+#     labels = averages.index
+#     plt.bar(labels, averages['Amount'])
+#     plt.title('Average Amount Spent in Each Category')
+#     plt.show()
 
-    # return json object
-    return json.dumps(income_response['accounts'][:2], indent=4, sort_keys=True)
+
+# # generate a pie chart of the number of transactions in each category
+# def pie_chart_counts(data):
+#     """
+#     Generates a pie chart of the number of transactions in each category
+#     """
+#     counts = get_category_counts(data)
+#     labels = counts.index
+#     plt.pie(counts['Amount'], labels=labels, autopct='%1.1f%%')
+#     plt.title('Number of Transactions in Each Category')
+#     plt.show()
+
+
+# # generate a bar chart of the number of transactions in each category
+# def bar_chart_counts(data):
+#     """
+#     Generates a bar chart of the number of transactions in each category
+#     """
+#     counts = get_category_counts(data)
+#     labels = counts.index
+#     plt.bar(labels, counts['Amount'])
+#     plt.title('Number of Transactions in Each Category')
+#     plt.show()
+
+
+# # generate a pie chart of the total amount spent in each category by month
+# def pie_chart_by_month(data):
+#     """
+#     Generates a pie chart of the total amount spent in each category by month
+#     """
+#     totals = data.groupby(['Category', 'Month']).sum()
+#     labels = totals.index
+#     plt.pie(totals['Amount'], labels=labels, autopct='%1.1f%%')
+#     plt.title('Total Amount Spent in Each Category by Month')
+#     plt.show()
+
+
+# # generate a bar chart of the total amount spent in each category by month
+# def bar_chart_by_month(data):
+#     """
+#     Generates a bar chart of the total amount spent in each category by month
+#     """
+#     totals = data.groupby(['Category', 'Month']).sum()
+#     labels = totals.index
+#     plt.bar(labels, totals['Amount'])
+#     plt.title('Total Amount Spent in Each Category by Month')
+#     plt.show()
